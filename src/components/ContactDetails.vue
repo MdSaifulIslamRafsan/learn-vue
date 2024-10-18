e39d8e87c53bd9a4ccecdd8dff7d7d6ab4301200
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-18 20:58:39 +0600

    learn how to create vue component

---
+++ src/components/ContactDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Name: {{name}}</h1>
+    <p> Number: {{ number }} </p>
+
+</template>
+
+<script>
+export default {
+    setup() {
+        return {
+            name: "test",
+            number: "123",
+        }
+    },
+}
+</script>
