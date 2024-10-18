31b0c52e48e4f3ea59340f247258e78a5cc65c47
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-18 21:14:14 +0600

    added style tag

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <h1>{{ msg }}</h1>
-  <ContactDetails></ContactDetails> 
+  <div class="grid grid-cols-3">
+    <ContactDetails />
+    <ContactDetails />
+    <ContactDetails />
+  </div>
 </template>
 
 <script>
-import ContactDetails from './components/ContactDetails.vue'
+import ContactDetails from "./components/ContactDetails.vue";
 export default {
   components: { ContactDetails },
   setup() {
     return {
-      msg: 'Hello Vue.js!',
-    }
+      msg: "Hello Vue.js!",
+    };
   },
   methods: {},
-}
+};
 </script>
--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,6 +1,9 @@
 <template>
+   <div class="card">
     <h1>Name: {{name}}</h1>
-    <p> Number: {{ number }} </p>
+    <p>Number: {{ number }} </p>
+    <p>website: www.lorem.com</p>
+   </div>
 
 </template>
 
@@ -14,3 +17,15 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+    .card{
+        border: 1px solid black;
+        padding: 20px;
+        margin: 10px;
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+        text-align: center;
+        overflow: hidden;
+ 
+    }
+</style>
