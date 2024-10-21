bdefee0dec8143fe086a79e2d483fc72a1f05466
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-22 01:17:58 +0600

    added  Multiple v-model support

--- src/App.vue
+++ src/App.vue
@@ -14,64 +14,67 @@
     <TheCard cardTitle="About me">
       <template v-slot:default>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
-        libero eget neque consequat, ac pulvinar enim congue. Nulla facilisi.
-        Donec vel velit et nunc lobortis fermentum. Sed et tellus non metus
-        sagittis placerat a vel mauris.
-      </p>
-      <a href="https://example.com" target="_blank">Visit my website</a>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
+          libero eget neque consequat, ac pulvinar enim congue. Nulla facilisi.
+          Donec vel velit et nunc lobortis fermentum. Sed et tellus non metus
+          sagittis placerat a vel mauris.
+        </p>
+        <a href="https://example.com" target="_blank">Visit my website</a>
 
-      <p>
-        <a href="mailto:john.doe@example.com">Contact me</a>
-      </p>
+        <p>
+          <a href="mailto:john.doe@example.com">Contact me</a>
+        </p>
 
-      <p>
-        <a href="https://github.com/johndoe" target="_blank"
-          >View my GitHub profile</a
-        >
-      </p>
+        <p>
+          <a href="https://github.com/johndoe" target="_blank"
+            >View my GitHub profile</a
+          >
+        </p>
 
-      <p>
-        <a href="https://www.linkedin.com/in/john-doe/" target="_blank"
-          >View my LinkedIn profile</a
-        >
-      </p>
+        <p>
+          <a href="https://www.linkedin.com/in/john-doe/" target="_blank"
+            >View my LinkedIn profile</a
+          >
+        </p>
 
-      <p>
-        <a href="https://twitter.com/johndoe" target="_blank"
-          >View my Twitter profile</a
-        >
-      </p>
+        <p>
+          <a href="https://twitter.com/johndoe" target="_blank"
+            >View my Twitter profile</a
+          >
+        </p>
       </template>
 
       <template v-slot:footer>
-        <a class="text-center block underline text-blue-500" href="">learn more</a>
+        <a class="text-center block underline text-blue-500" href=""
+          >learn more</a
+        >
       </template>
-      
     </TheCard>
     <TheCard cardTitle="iphone 12 pro max">
+      <template #default>
+        <img
+          class="h-60 mx-auto"
+          src="https://adminapi.applegadgetsbd.com/storage/media/large/2445-37576.jpg"
+          alt="iphone 12 pro max"
+        />
+        <p>
+          The iPhone 12 Pro Max is a smartphone designed and marketed by Apple
+          Inc. It was unveiled on September 14, 2020, and is the third iPhone in
+          the series since the iPhone 11. The smartphone features a 6.1-inch
+          Super Retina XDR OLED display, a 12MP (f/1.7) telephoto camera with O
+        </p>
+      </template>
 
-
-    <template #default>
-
-      <img class="h-60 mx-auto"
-        src="https://adminapi.applegadgetsbd.com/storage/media/large/2445-37576.jpg"
-        alt="iphone 12 pro max"
-      />
-      <p>
-        The iPhone 12 Pro Max is a smartphone designed and marketed by Apple Inc. It
-        was unveiled on September 14, 2020, and is the third iPhone in the series
-        since the iPhone 11. The smartphone features a 6.1-inch Super Retina XDR OLED
-        display, a 12MP (f/1.7) telephoto camera with O </p>
-    </template>
-
-    <template #footer>
-     <div class="flex justify-center">
-         <button class="text-center py-2 mx-4 px-5 bg-blue-500" href="">buy now</button>
-      <button class="text-center py-2 mx-4 px-5 bg-blue-500" href="">Add to card</button>
-     </div>
-    </template>
-
+      <template #footer>
+        <div class="flex justify-center">
+          <button class="text-center py-2 mx-4 px-5 bg-blue-500" href="">
+            buy now
+          </button>
+          <button class="text-center py-2 mx-4 px-5 bg-blue-500" href="">
+            Add to card
+          </button>
+        </div>
+      </template>
     </TheCard>
     <TheCard cardTitle="services">
       <ul>
@@ -80,7 +83,9 @@
         <li>design</li>
       </ul>
       <p>
-        <a href="https://example.com/services" target="_blank">View our services</a>
+        <a href="https://example.com/services" target="_blank"
+          >View our services</a
+        >
       </p>
       <p>
         <a href="https://example.com/contact" target="_blank">Contact us</a>
@@ -98,46 +103,72 @@
         <a href="https://example.com/careers" target="_blank">Careers</a>
       </p>
     </TheCard>
-    <TheCard cardTitle="task">
-    </TheCard>
-      <Count></Count>
+    <TheCard cardTitle="task"> </TheCard>
+    <Count></Count>
   </div>
-    <div class="grid lg:grid-cols-2">
-        <ProductCard v-for="(product) in products" :key="product.id" :product="product" @buy-now-click="buyNow" @add-to-card-click="addNow" @toggle-favourite="toggleFavourite"></ProductCard> 
-    </div>
-    <p>{{myRating}}</p>
-    <the-rating v-model="myRating"></the-rating>
-   <CreditCard></CreditCard>
+  <div class="grid lg:grid-cols-2">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+      @buy-now-click="buyNow"
+      @add-to-card-click="addNow"
+      @toggle-favourite="toggleFavourite"
+    ></ProductCard>
+  </div>
+  <p>{{ myRating }}</p>
+  <the-rating v-model="myRating"></the-rating>
+  <div>
+    <CreditCard v-model:cardHolderName="cardHolderName" v-model:cardNumber="cardNumber" v-model:expirationDate="expirationDate" v-model:cvv="cvv"> </CreditCard>
+    <br />
+    <br />
+    <hr />
+    <hr />
+    <hr />
+    <p>
+      name : {{ cardHolderName }} card number :
+      {{ cardNumber }} expiration date :
+      {{ expirationDate }} cvc : {{ cvv }}
+    </p>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 import ContactDetails from "./components/ContactDetails.vue";
-import Count from './components/Count.vue';
-import ProductCard from './components/ProductCard.vue';
+import Count from "./components/Count.vue";
+import ProductCard from "./components/ProductCard.vue";
 import TheCard from "./components/TheCard.vue";
-import TheRating from './components/TheRating.vue';
-import CreditCard from './components/CreditCard.vue';
+import TheRating from "./components/TheRating.vue";
+import CreditCard from "./components/CreditCard.vue";
 export default {
-  components: { ContactDetails, TheCard, Count, ProductCard, TheRating, CreditCard },
-  methods:{
-      buyNow(product) {
-        console.log("Buy Now clicked!", product);
-      },
-      addNow(product){
-        console.log("Add to Card clicked!", product);
-      },
-     toggleFavourite(product){
-      console.log(product);
-      
+  components: {
+    ContactDetails,
+    TheCard,
+    Count,
+    ProductCard,
+    TheRating,
+    CreditCard,
+  },
+  methods: {
+    buyNow(product) {
+      console.log("Buy Now clicked!", product);
+    },
+    addNow(product) {
+      console.log("Add to Card clicked!", product);
+    },
+    toggleFavourite(product) {
       product.addToFavourite = !product.addToFavourite;
-     }
-
+    },
   },
   setup() {
     return {
       myRating: ref(2),
       msg: "Hello Vue.js!",
+      cardNumber: ref(""),
+      cardHolderName: ref(""),
+      expirationDate:ref(""),
+      cvv: ref(""),
       contact1: {
         name: "John Doe",
         phone: "123-456-7890",
@@ -176,29 +207,33 @@ export default {
           id: 1,
           name: "iPhone 12 Pro Max",
           price: 10000,
-          description: "The iPhone 12 Pro Max is a smartphone designed and marketed by Apple Inc. It was unveiled on September 14, 2020, and is the third iPhone in the series since the iPhone 11.",
-          image: "https://adminapi.applegadgetsbd.com/storage/media/large/2445-37576.jpg",
+          description:
+            "The iPhone 12 Pro Max is a smartphone designed and marketed by Apple Inc. It was unveiled on September 14, 2020, and is the third iPhone in the series since the iPhone 11.",
+          image:
+            "https://adminapi.applegadgetsbd.com/storage/media/large/2445-37576.jpg",
           addToFavourite: true,
         },
         {
           id: 2,
           name: "Samsung Galaxy S21 Ultra",
           price: 9000,
-          description: "The Samsung Galaxy S21 Ultra is a smartphone developed by Samsung Electronics. It was announced on September 12, 2020, and is the fourth Samsung",
-          image: "https://images.samsung.com/is/image/samsung/p6pim/levant/2401/gallery/levant-galaxy-s24-s928-sm-s928bztqmea-thumb-539426017",
-           addToFavourite: false,
+          description:
+            "The Samsung Galaxy S21 Ultra is a smartphone developed by Samsung Electronics. It was announced on September 12, 2020, and is the fourth Samsung",
+          image:
+            "https://images.samsung.com/is/image/samsung/p6pim/levant/2401/gallery/levant-galaxy-s24-s928-sm-s928bztqmea-thumb-539426017",
+          addToFavourite: false,
         },
         {
           id: 3,
           name: "Samsung Galaxy S21 Ultra",
           price: 9000,
-          description: "The Samsung Galaxy S21 Ultra is a smartphone developed by Samsung Electronics. It was announced on September 12, 2020, and is the fourth Samsung",
-          image: "https://images.samsung.com/is/image/samsung/p6pim/levant/2401/gallery/levant-galaxy-s24-s928-sm-s928bztqmea-thumb-539426017",
-           addToFavourite: true,
-        }
-
-      ])
-      
+          description:
+            "The Samsung Galaxy S21 Ultra is a smartphone developed by Samsung Electronics. It was announced on September 12, 2020, and is the fourth Samsung",
+          image:
+            "https://images.samsung.com/is/image/samsung/p6pim/levant/2401/gallery/levant-galaxy-s24-s928-sm-s928bztqmea-thumb-539426017",
+          addToFavourite: true,
+        },
+      ]),
     };
   },
 };
--- src/components/CreditCard.vue
+++ src/components/CreditCard.vue
@@ -2,20 +2,51 @@
     <div>
         <h1>Credit Card</h1>
         <div class="card">
-          
+          <label>
+            Name on Card
+          </label>
+          <input class="border-2" @input="handleCardHolderName" :value="cardHolderName" type="text" />
+          <input class="border-2" type="text" :value="cardNumber" @input="handleCardNumber"  />
+          <input class="border-2" type="text" :value="expirationDate" @input="handleExpirationDate" />
+          <input class="border-2" type="text" :value="cvv" @input="handleCvv" />
         </div>
     </div>
 </template>
 <script>
 export default {
-    setup: () => {
-        return {
-            cardNumber: '',
-            cardHolderName: '',
-            expirationDate: '',
-            cvv: ''
+    props: {
+        cardHolderName: {
+            type: String,
+            default : "",
+        },
+        cardNumber: {
+            type: Number,
+            default : "",
+        },
+        expirationDate: {
+            type: String,
+            default : "",
+        },
+        cvv: {
+            type: String,
+            default : "",
         }
-    }
+    },
+    methods: {
+        handleCardHolderName(e){
+           this.$emit("update:cardHolderName", e.target.value);
+        },
+        handleCardNumber(e){
+           this.$emit("update:cardNumber", e.target.value);
+        },
+        handleExpirationDate(e){
+           this.$emit("update:expirationDate", e.target.value);
+        },
+        handleCvv(e){
+           this.$emit("update:cvv", e.target.value);
+        },
+    },
+   
 }
 </script>
 <style  scoped>
